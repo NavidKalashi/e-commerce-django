38f99b16dff0affc3ff2c72abe3dab3153d7cfb1
Navid Kalashi <kalashinavid@gmail.com>
2025-08-28 16:59:46 +0330

    advance api

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'rest_framework',
     'store',
     'store_custom',
@@ -132,5 +133,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'COERCE_DECIMAL_TO_STRING': False
+    'COERCE_DECIMAL_TO_STRING': False,
 }
